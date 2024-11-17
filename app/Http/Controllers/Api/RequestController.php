b477136da4712f9a2ef756e4cb56ba77693dcb5d
XomyachokAndrew <124427665+XomyachokAndrew@users.noreply.github.com>
2024-11-17 22:50:09 +0300

    Add controller for Requests

---
+++ app/Http/Controllers/Api/RequestController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\RequestResources;
+use App\Models\Requests;
+use Illuminate\Http\Request;
+
+class RequestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return RequestResources::collection(Requests::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $req)
+    {
+        $requests = Requests::create($req->all());
+
+        return new RequestResources($requests);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return new RequestResources(Requests::findOrFail($id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $req, Requests $requests)
+    {
+        $requests->update($req->validated());
+
+        return new RequestResources($requests);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Requests $requests)
+    {
+        $requests->delete();
+
+        return response(null, 204);
+    }
+}
