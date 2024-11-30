6d5bec11354c0a450071992df4bdc18b71ef6882
XomyachokAndrew <124427665+XomyachokAndrew@users.noreply.github.com>
2024-11-30 16:01:17 +0300

    Layout with context

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Header from "./components/Header";
+import { AuthProvider } from "./context/AuthContext";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -20,11 +22,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <AuthProvider>
+          <Header />
+          {children}
+        </AuthProvider>
       </body>
+
     </html>
   );
 }
