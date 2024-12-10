4ea6455d801841d689a9ef59e228112e7d709986
XomyachokAndrew <124427665+XomyachokAndrew@users.noreply.github.com>
2024-12-10 20:31:43 +0300

    Added delete methods

--- app/api/handlers.js
+++ app/api/handlers.js
@@ -6,7 +6,10 @@ const csrf_token = async () => {
     const response = await axios.get(`/csrf-token`, { withCredentials: true });
     return response.data.csrfToken;
 }
+
 //#region GET
+
+//#region Fish
 export const getFishNames = async () => {
     try {
         const response = await axios.get('/fish_names');
@@ -46,7 +49,9 @@ export const getFishUser = async (id) => {
         console.error('qwe', error);
     }
 };
+//#endregion
 
+//#region Request
 export const getRequests = async () => {
     try {
         const response = await axios.get(`/requests`);
@@ -76,7 +81,9 @@ export const getRequestUser = async (id) => {
         console.error('qwe', error);
     }
 };
+//#endregion
 
+//#region Order
 export const getOrderUser = async (id) => {
     try {
         const response = await axios.get(`/user/orders/${id}`);
@@ -90,6 +97,33 @@ export const getOrderUser = async (id) => {
 export const getOrderFish = async (id) => {
     try {
         const response = await axios.get(`/fish/orders/${id}`);
+        const options = response.data.data;
+        return options;
+    } catch (error) {
+        console.error('qwe', error);
+    }
+};
+//#endregion
+
+export const getUsers = async () => {
+    try {
+        const token = localStorage.getItem('token');
+        const response = await axios.get(`/users`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+            withCredentials: true,
+        });
+        const options = response.data.data;
+        return options;
+    } catch (error) {
+        console.error('qwe', error);
+    }
+};
+
+export const getPhotosFish = async (id) => {
+    try {
+        const response = await axios.get(`/fish/photos/${id}`);
         const options = response.data.data;
         return options;
     } catch (error) {
@@ -167,4 +201,104 @@ export const postOrder = async (order) => {
         throw error; // Пробрасываем ошибку для дальнейшей обработки
     }
 };
+//#endregion
+
+//#region DELETE
+
+export const deleteUser = async (id) => {
+    try {
+        const csrfToken = await csrf_token();
+        const token = localStorage.getItem('token');
+        const response = await axios.delete(`/users/${id}`, {
+            headers: {
+                'X-CSRF-TOKEN': csrfToken,
+                Authorization: `Bearer ${token}`,
+            },
+            withCredentials: true,
+        });
+
+        return response.data; // Возвращаем данные о созданной рыбе и загруженных фотографиях
+    } catch (error) {
+        console.error('Ошибка при удалении User:', error.response?.data || error.message);
+        throw error; // Пробрасываем ошибку для дальнейшей обработки
+    }
+}
+
+export const deleteFish = async (id) => {
+    try {
+        const csrfToken = await csrf_token();
+        const token = localStorage.getItem('token');
+        const response = await axios.delete(`/fishes/${id}`, {
+            headers: {
+                'X-CSRF-TOKEN': csrfToken,
+                Authorization: `Bearer ${token}`,
+            },
+            withCredentials: true,
+        });
+        console.log(response);
+
+        return response.data; // Возвращаем данные о созданной рыбе и загруженных фотографиях
+    } catch (error) {
+        console.error('Ошибка при удалении Fish:', error.response?.data || error.message);
+        throw error; // Пробрасываем ошибку для дальнейшей обработки
+    }
+}
+
+export const deleteOrder = async (id) => {
+    try {
+        const csrfToken = await csrf_token();
+        const token = localStorage.getItem('token');
+        const response = await axios.delete(`/orders/${id}`, {
+            headers: {
+                'X-CSRF-TOKEN': csrfToken,
+                Authorization: `Bearer ${token}`,
+            },
+            withCredentials: true,
+        });
+
+        return response.data; // Возвращаем данные о созданной рыбе и загруженных фотографиях
+    } catch (error) {
+        console.error('Ошибка при удалении Order:', error.response?.data || error.message);
+        throw error; // Пробрасываем ошибку для дальнейшей обработки
+    }
+}
+
+export const deletePhoto = async (id) => {
+    try {
+        const csrfToken = await csrf_token();
+        const token = localStorage.getItem('token');
+        const response = await axios.delete(`/photos/${id}`, {
+            headers: {
+                'X-CSRF-TOKEN': csrfToken,
+                Authorization: `Bearer ${token}`,
+            },
+            withCredentials: true,
+        });
+
+        return response.data; // Возвращаем данные о созданной рыбе и загруженных фотографиях
+    } catch (error) {
+        console.error('Ошибка при удалении Photo:', error.response?.data || error.message);
+        throw error; // Пробрасываем ошибку для дальнейшей обработки
+    }
+}
+
+export const deleteRequest = async (id) => {
+    try {
+        const csrfToken = await csrf_token();
+        const token = localStorage.getItem('token');
+        const response = await axios.delete(`/requests/${id}`, {
+            headers: {
+                'X-CSRF-TOKEN': csrfToken,
+                Authorization: `Bearer ${token}`,
+            },
+            withCredentials: true,
+        });
+
+        return response.data; // Возвращаем данные о созданной рыбе и загруженных фотографиях
+    } catch (error) {
+        console.error('Ошибка при удалении Request:', error.response?.data || error.message);
+        throw error; // Пробрасываем ошибку для дальнейшей обработки
+    }
+}
+
 //#endregion
