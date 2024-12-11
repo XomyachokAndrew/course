88696eb81dd9c1d65ccbdde6dc8b47e5aa342af8
XomyachokAndrew <124427665+XomyachokAndrew@users.noreply.github.com>
2024-12-11 23:17:50 +0300

    Modify catalog

--- app/(pages)/catalog/page.js
+++ app/(pages)/catalog/page.js
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import FishCard from "@/app/components/FishCard";
 import RequestCard from "@/app/components/RequestCard"; // Импортируйте компонент для отображения запросов
@@ -9,6 +9,7 @@ const Catalog = () => {
     const [fishes, setFishes] = useState([]);
     const [requests, setRequests] = useState([]);
     const [showFishes, setShowFishes] = useState(true); // Состояние для переключения между рыбами и запросами
+    const [selectedType, setSelectedType] = useState('Все'); // Состояние для выбранного типа рыбы
 
     useEffect(() => {
         const fetchFishes = async () => {
@@ -25,52 +26,81 @@ const Catalog = () => {
         fetchRequests();
     }, []);
 
+    // Фильтрация рыб по типу
+    const filteredFishes = selectedType === 'Все' ? fishes : fishes.filter(fish => fish.type === selectedType);
+
     return (
-        <div>
-            <div className="flex items-center justify-center min-h-screen mt-16">
-                <div className="max-w-4xl mx-auto p-6 bg-white rounded-lg shadow-md mt-16">
-                    <div className="flex justify-between mb-4">
-                        <button
-                            onClick={() => setShowFishes(true)}
-                            className={`py-2 px-4 rounded-lg ${showFishes ? 'bg-blue-600 text-white' : 'bg-gray-200 text-gray-800'}`}
-                        >
-                            Рыбы
-                        </button>
-                        <button
-                            onClick={() => setShowFishes(false)}
-                            className={`py-2 px-4 rounded-lg ${!showFishes ? 'bg-blue-600 text-white' : 'bg-gray-200 text-gray-800'}`}
-                        >
-                            Запросы
-                        </button>
+        <div className="min-h-screen flex items-center justify-center mt-16">
+            <div className="max-w-4xl mx-auto p-6 bg-white rounded-lg shadow-md">
+                <div className="flex justify-between mb-4">
+                    <button
+                        onClick={() => setShowFishes(true)}
+                        className={`py-2 px-4 rounded-lg ${showFishes ? 'bg-blue-600 text-white' : 'bg-gray-200 text-gray-800'}`}
+                    >
+                        Рыбы
+                    </button>
+                    <button
+                        onClick={() => setShowFishes(false)}
+                        className={`py-2 px-4 rounded-lg ${!showFishes ? 'bg-blue-600 text-white' : 'bg-gray-200 text-gray-800'}`}
+                    >
+                        Запросы
+                    </button>
+                </div>
+                {showFishes && (
+                    <div className="mb-4">
+                        <h2 className="text-lg font-semibold mb-2">Фильтровать по типу:</h2>
+                        <div className="flex flex-wrap space-x-2">
+                            {['Все', 'Живая', 'Охлажденная', 'Мороженная', 'Соленая', 'Сушеная', 'Жаренная'].map(type => (
+                                <button
+                                    key={type}
+                                    onClick={() => setSelectedType(type)}
+                                    className={`py-1 px-3 rounded-lg ${selectedType === type ? 'bg-blue-600 text-white' : 'bg-gray-200 text-gray-800'}`}
+                                >
+                                    {type === 'all' ? 'Все' : type.charAt(0).toUpperCase() + type.slice(1)}
+                                </button>
+                            ))}
+                        </div>
                     </div>
-                    <div className="mt-6">
-                        <h1 className="text-2xl font-semibold mb-2">{showFishes ? 'Рыбы' : 'Запросы'}</h1>
-                        {showFishes ? (
-                            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-                                {fishes.length > 0 ? (
-                                    fishes.slice().reverse().map((fish) => (
-                                        <FishCard key={fish.id} fish={fish} />
-                                    ))
-                                ) : (
-                                    <p className="text-gray-500">Нет рыб.</p>
-                                )}
-                            </div>
-                        ) : (
-                            <div className="flex flex-col">
-                                {requests.length > 0 ? (
-                                    requests.map((request) => (
-                                        <RequestCard key={request.id} request={request} /> // Используйте компонент для отображения запросов
-                                    ))
-                                ) : (
-                                    <p className="text-gray-500">Нет запросов.</p>
-                                )}
-                            </div>
-                        )}
-                    </div>
+                )}
+                <div className="mt-6">
+                    <h1 className="text-2xl font-semibold mb-2">{showFishes ? 'Рыбы' : 'Запросы'}</h1>
+                    {showFishes ? (
+                        <FishSection fishes={filteredFishes} />
+                    ) : (
+                        <RequestSection requests={requests} />
+                    )}
                 </div>
             </div>
         </div>
     );
+};
+
+const FishSection = ({ fishes }) => {
+    return (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            {fishes.length > 0 ? (
+                fishes.slice().reverse().map((fish) => (
+                    <FishCard key={fish.id} fish={fish} />
+                ))
+            ) : (
+                <p className="text-gray-500">Нет рыб.</p>
+            )}
+        </div>
+    )
+};
+
+const RequestSection = ({ requests }) => {
+    return (
+        <div className="flex flex-col">
+            {requests.length > 0 ? (
+                requests.map((request) => (
+                    <RequestCard key={request.id} request={request} />
+                ))
+            ) : (
+                <p className="text-gray-500">Нет запросов.</p>
+            )}
+        </div>
+    )
 };
 
 export default Catalog;
